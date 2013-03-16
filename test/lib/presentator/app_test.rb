fb1b9aa8b8773cbf88a61c6cba83f209c6a84711
Ivan Zarea <zarea.ion@gmail.com>
2013-03-16 18:51:10 +0200

    Failing test with some added slides

--- test/lib/presentator/app_test.rb
+++ test/lib/presentator/app_test.rb
@@ -1,14 +1,31 @@
 require_relative '../../test_helper'
 
 describe Presentator::App do
-  before do
-    @app = Presentator::App.new
-  end
 
   describe "With no slides set" do
+    before do
+      @app = Presentator::App.new
+    end
+
     it "should not be able to run" do
+      @app = Presentator::App.new
       present = -> { @app.present! }
       present.must_raise Presentator::NoContentError
+    end
+  end
+
+  describe "With an added slide" do
+    before do
+      @app = Presentator::App.new do
+        slide do
+          text "Hello, world"
+        end
+      end
+    end
+
+    it "must have slides with some content" do
+      @app.slides.wont_be_empty
+      -> { @app.present }.wont_raise Presentator::NoContentError
     end
   end
 end
