e86e7a9e663d88bc03e26955845c18dffd88c242
Ivan Zarea <zarea.ion@gmail.com>
2013-03-16 19:27:13 +0200

    A simple DSL for slides and texts. Prints stuff for now

--- lib/presentator/app.rb
+++ lib/presentator/app.rb
@@ -1,14 +1,40 @@
-
 module Presentator
   class App
     attr_accessor :slides
 
     def initialize(opts={}, &blk)
       @slides = []
+
+      instance_eval &blk
     end
 
     def present!
       raise NoContentError if @slides.empty?
+      @slides.each {|s| s.show}
+    end
+
+    def slide(&blk)
+      @slides << Slide.new(&blk)
+    end
+
+  end
+
+  class Slide
+    attr_accessor :elements
+
+    def initialize(opts={}, &blk)
+      @elements = []
+
+      instance_eval &blk
+    end
+
+    def text(message="")
+      @elements << message
+    end
+
+    def show
+      @elements.each {|e| p e}
     end
   end
+
 end
--- test/lib/presentator/app_test.rb
+++ test/lib/presentator/app_test.rb
@@ -4,11 +4,11 @@ describe Presentator::App do
 
   describe "With no slides set" do
     before do
-      @app = Presentator::App.new
+      @app = Presentator::App.new do
+      end
     end
 
     it "should not be able to run" do
-      @app = Presentator::App.new
       present = -> { @app.present! }
       present.must_raise Presentator::NoContentError
     end
@@ -25,7 +25,6 @@ describe Presentator::App do
 
     it "must have slides with some content" do
       @app.slides.wont_be_empty
-      -> { @app.present }.wont_raise Presentator::NoContentError
     end
   end
 end
