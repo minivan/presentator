849a348358280642002e097140d6a8e094643fb1
Ivan Zarea <zarea.ion@gmail.com>
2013-03-17 23:57:31 +0200

    slide texts are now shown with curses. cool-cool-cool

--- lib/presentator/slide.rb
+++ lib/presentator/slide.rb
@@ -1,6 +1,8 @@
+require 'curses'
 
 module Presentator
   class Slide
+    include Curses
     attr_accessor :elements
 
     def initialize(opts={}, &blk)
@@ -13,7 +15,34 @@ module Presentator
     end
 
     def show
-      @elements.each {|e| p e}
+      @elements.each_with_index do |element, index|
+        show_text element, offset: -index*2
+      end
+    end
+
+    private
+
+    def show_text(message, params={})
+      offset = params[:offset] || 0
+      position = params[:position] || :center
+
+      width = message.length + 6
+      pos = [1, width]
+
+      case position
+      when :top
+        pos += [3, (cols - width)/ 2]
+      when :center
+        pos += [(lines - offset) / 2, (cols - width) / 2]
+      end
+
+      win = Window.new(*pos)
+      win.setpos(0, 0)
+      win.addstr(message)
+      win.refresh
+      win.close
+
+      win
     end
   end
 end
