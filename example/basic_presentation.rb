020183a65dd87cb5dc61f05af94e31dcd8ba74b9
Ivan Zarea <zarea.ion@gmail.com>
2013-03-18 23:16:37 +0200

    Updated the example to have two slides now

--- example/basic_presentation.rb
+++ example/basic_presentation.rb
@@ -6,6 +6,10 @@ app = Presentator::App.new do
     text "this is harmless"
     text "it prints stuff for now, but that's fine"
   end
+  slide do
+    text "Hello there"
+    text "this is a second slide"
+  end
 end
 
 app.present!
