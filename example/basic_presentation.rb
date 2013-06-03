51d1d67b48bbb8ea99d61b60c32c94164dcb8c7e
Ivan Zarea <zarea.ion@gmail.com>
2013-06-03 19:28:35 +0300

    Added an example of how the lists should look like

--- example/basic_presentation.rb
+++ example/basic_presentation.rb
@@ -2,13 +2,20 @@ require 'presentator'
 
 app = Presentator::App.new do
   slide do
-    text "Hello there"
-    text "this is harmless"
-    text "it prints stuff for now, but that's fine"
+    text 'Hello there'
+    text 'this is harmless'
+    text 'it prints stuff for now, but that\'s fine'
   end
   slide do
-    text "Hello there"
-    text "this is a second slide"
+    list do
+      text 'This is the first element of the list'
+      text 'This is the second one'
+      text 'Cool, eh?'
+    end
+  end
+  slide do
+    text 'Hello there'
+    text 'this is the second slide'
   end
 end
 
