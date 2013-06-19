2bf71750343616d5812b586d1795cc3d1ce24390
Ivan Zarea <zarea.ion@gmail.com>
2013-06-19 23:27:18 +0300

    :arrow_double_down: The slide will also be an element

--- lib/presentator.rb
+++ lib/presentator.rb
@@ -1,8 +1,9 @@
 require 'presentator/app'
 require 'presentator/exceptions'
-require 'presentator/slide'
 require 'presentator/elements/base'
+require 'presentator/elements/slide'
 require 'presentator/elements/text'
+require 'presentator/elements/list'
 require 'presentator/version'
 module Presentator
 end
