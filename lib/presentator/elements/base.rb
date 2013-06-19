7bac04fb09fa4b337d8e3a1c3fae98df682552b6
Ivan Zarea <zarea.ion@gmail.com>
2013-06-19 23:28:37 +0300

    :clock230: dis gon be gud, Base has everything an element wants to have

--- lib/presentator/elements/base.rb
+++ lib/presentator/elements/base.rb
@@ -3,8 +3,32 @@ require 'curses'
 module Presentator
   class Base
     include Curses
+    attr_accessor :elements
 
-    def show
+    def initialize(opts = {}, &blk)
+      @elements = []
+      instance_eval &blk
+    end
+
+    def text(message = '', opts = {})
+      elements << Text.new(message, opts)
+    end
+
+    def list(opts = {}, &blk)
+      elements << List.new(opts, &blk)
+    end
+
+    def show(opts = {})
+      raise NotImplementedError,
+            "The container #{self.class.name} has not implemented a show method!"
+    end
+
+    def height
+      elements.map(&:height).inject(:+)
+    end
+
+    def width
+      elements.map(&:width).inject(:+)
     end
   end
 end
