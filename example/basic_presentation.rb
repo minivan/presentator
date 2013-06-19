72eb6994d458910a35c74798f217d4a45e15a019
Ivan Zarea <zarea.ion@gmail.com>
2013-06-19 23:31:16 +0300

    :ok_woman: MOAR EXAMPLES

--- example/basic_presentation.rb
+++ example/basic_presentation.rb
@@ -6,16 +6,21 @@ app = Presentator::App.new do
     text 'this is harmless'
     text 'it prints stuff for now, but that\'s fine'
   end
-#  slide do
-#    list do
-#      text 'This is the first element of the list'
-#      text 'This is the second one'
-#      text 'Cool, eh?'
-#    end
-#  end
   slide do
-    text 'Hello there'
-    text 'this is the second slide'
+    list do
+      text '1. This is the first element of the list'
+      text '2. This is the second one'
+      text '3. Cool, eh?'
+      text '4. Cool, eh-eh?'
+      text '5. This is the first element of the list'
+      text '6. This is the second one'
+      text '7. This is the first element of the list'
+      text '8. This is the second one'
+    end
+  end
+  slide do
+    text 'Hello there', position: :top
+    text 'this is the third slide'
   end
 end
 
